e47e36a4dbd6ef12dd33d338a8a15abc2db245bd
sawalhiw <waleed.sawalhi1@gmail.com>
2024-08-29 16:23:06 +0300

    Add missing validation messages.

--- src/main/java/org/bank/service/implmentation/TokenValidationServiceImpl.java
+++ src/main/java/org/bank/service/implmentation/TokenValidationServiceImpl.java
@@ -4,11 +4,9 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bank.dto.DefaultResponseDto;
 import org.bank.service.TokenValidationService;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 
 import static org.bank.utils.util.JwtUtils.SECRET_KEY;
 
